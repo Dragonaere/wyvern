0c4517fd3cb07eecc62c0d1a42ccf69f08e1d668
AndrewDragon <andrew@andrewdragon.dev>
2023-02-11 18:45:35 -0500

    Added more meta and messed with CSS

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,16 @@ export const metadata = {
   title: {
     default: 'Wyvern',
     template: '%s - Wyvern'
-  }
+  },
+  generator: 'Next.js',
+  applicationName: 'Wyvern',
+  referrer: 'origin-when-cross-origin',
+  colorScheme: 'dark',
+  formatDetection: {
+    email: 'no',
+    address: 'no',
+    telephone: 'no',
+  },
 };
 
 export default function RootLayout({ children }) {
