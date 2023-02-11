b755313d73237da994da0bcc3501c5db7364fd92
AndrewDragon <andrew@andrewdragon.dev>
2023-02-11 17:56:41 -0500

    fixed

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -2,11 +2,10 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export async function generateMetadata(a) {
-  console.log(a);
-  console.log(a.searchParams);
-  console.log(a.searchParams.search_query);
   return { title: `${a.searchParams.search_query} - Wyvern`, }
 }
+
+export const runtime = 'nodejs';
 
 export default function Page() {
   return (
